e0686bb1c2e7226731e18f1cb75a3f0562aab6ca
satyamdudhat <satyam.glsica19@gmail.com>
2024-02-28 18:45:34 +0530

    Update yaml file

--- Dockerfile
+++ Dockerfile
@@ -1,27 +1,7 @@
-# FROM python:3.8-slim-arm64
-
-# COPY requirements.txt ./
-# COPY myfunction.py ./
-
-# RUN echo "Listing files:" && ls -l  
-# RUN cat requirements.txt  
-
-
-# RUN pip3 install -r requirements.txt
-# CMD ["myfunction.lambda_handler"]
-
-
-FROM python:3.8-slim 
-
-WORKDIR /app
+FROM public.ecr.aws/lambda/python:3.8
 
 COPY requirements.txt ./
 RUN pip3 install -r requirements.txt
-
 COPY myfunction.py ./
-
-# Optional for debugging:
-RUN echo "Listing files:" && ls -l
-RUN cat requirements.txt
 
 CMD ["myfunction.lambda_handler"]
--- ECR_Code.tf
+++ ECR_Code.tf
@@ -4,36 +4,11 @@ resource "aws_ecr_repository" "satyam_project" {
   image_tag_mutability = "IMMUTABLE" #using this we can make a private Registry
 }
 
-
-
-
-
-
-
-# resource "null_resource" "push_docker_image_to_ecr" {
-#   provisioner "local-exec" {
-#     command = <<EOT
-#       aws ecr get-login-password --region ap-south-1 --profile satyam | docker login --username AWS --password-stdin 637423557273.dkr.ecr.ap-south-1.amazonaws.com
-#       docker build -t satyam_project --platform linux/arm64 .
-#       docker tag satyam_project 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:latest
-#       docker push 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:latest
-#     EOT
-#   }
-# }
-
 # Code used to deploy an image to ECR using a shell file
-# resource "null_resource" "push_docker_image_to_ecr" {
+resource "null_resource" "push_docker_image_to_ecr" {
 
-#   depends_on = [aws_ecr_repository.satyam_project]
-#   provisioner "local-exec" {
-#     command = "bash ./scripts/run_command.sh"
-#   }
-# } 
-
-# resource "null_resource" "build_and_push_docker_image" {
-#   # triggers = {
-#   #   # You can set triggers here if needed
-#   # }
-  
-  
-# }
+  depends_on = [aws_ecr_repository.satyam_project]
+  provisioner "local-exec" {
+    command = "bash ./scripts/run_command.sh"
+  }
+} 
