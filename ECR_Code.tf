0dae69288f7da09be7ff9d0077d92cb5042551be
satyamdudhat <satyam.glsica19@gmail.com>
2024-02-28 16:53:44 +0530

    Update yaml file

--- ECR_Code.tf
+++ ECR_Code.tf
@@ -2,6 +2,16 @@
 resource "aws_ecr_repository" "satyam_project" {
   name = var.ecr_name
   image_tag_mutability = "IMMUTABLE" #using this we can make a private Registry
+
+  provisioner "local-exec" {
+    command = <<EOT
+      aws ecr get-login-password --region ap-south-1 --profile satyam | docker login --username AWS --password-stdin 637423557273.dkr.ecr.ap-south-1.amazonaws.com
+      docker build -t satyam_project --platform linux/arm64 .
+      docker tag satyam_project 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:latest
+      docker push 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:latest
+
+    EOT
+  }
 }
 
 # Code used to deploy an image to ECR using a shell file
@@ -13,24 +23,10 @@ resource "aws_ecr_repository" "satyam_project" {
 #   }
 # }
 
-resource "null_resource" "build_and_push_docker_image" {
-  # triggers = {
-  #   # You can set triggers here if needed
-  # }
+# resource "null_resource" "build_and_push_docker_image" {
+#   # triggers = {
+#   #   # You can set triggers here if needed
+#   # }
   
-  provisioner "local-exec" {
-    command = <<EOT
-      # Log in to Amazon ECR
-      aws ecr get-login-password --region ap-south-1 --profile satyam | docker login --username AWS --password-stdin 637423557273.dkr.ecr.ap-south-1.amazonaws.com
-
-      # Build the Docker image
-      docker build -t satyam_project --platform linux/arm64 .
-
-      # Tag the Docker image
-      docker tag satyam_project 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:latest
-
-      # Push the Docker image to Amazon ECR
-      docker push 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:latest
-    EOT
-  }
-}
+  
+# }
