150fffc2413fa3d13a801073bb662862fe76b76f
satyamdudhat <satyam.glsica19@gmail.com>
2024-02-29 10:18:16 +0530

    Update yaml file

--- output_url.tf
+++ output_url.tf
@@ -1,3 +1,3 @@
-output "endpoint_url" {
-  value = "${aws_api_gateway_stage.satyam_stage.invoke_url}/${var.endpoint_path}"
-}
+# output "endpoint_url" {
+#   value = "${aws_api_gateway_stage.satyam_stage.invoke_url}/${var.endpoint_path}"
+# }
