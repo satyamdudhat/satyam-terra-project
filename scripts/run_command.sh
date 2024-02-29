4bfe9d2d586dbbb8fbbefdde30e19db84cc89ce9
satyamdudhat <satyam.glsica19@gmail.com>
2024-02-29 10:35:46 +0530

    Update yaml file

--- scripts/run_command.sh
+++ scripts/run_command.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 aws ecr get-login-password --region ap-south-1 --profile satyam | docker login --username AWS --password-stdin 637423557273.dkr.ecr.ap-south-1.amazonaws.com
-docker build -t satyam_project --platform=linux/arm64 .
-docker tag satyam_project 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:latest
+docker build -t satyam_project:latest --platform=linux/arm64 .
+docker tag satyam_project:latest 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:latest
 docker push 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:latest
 
 
