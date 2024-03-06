3e8ce11b8b293b2ad376962823c0712c26b37a73
satyamdudhat <satyam.glsica19@gmail.com>
2024-03-06 18:42:16 +0530

    Update a status Resource code

--- api_gateway.tf
+++ api_gateway.tf
@@ -40,10 +40,29 @@ resource "aws_lambda_permission" "apigw_lambda" {
   source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.serverless_demos.id}/*/${aws_api_gateway_method.serverless_demos_method.http_method}${aws_api_gateway_resource.serverless_demos_resource.path}"
 }
 
+# ---------------------CoRS Integration
+resource "aws_api_gateway_method_response" "method_response1" {
+  rest_api_id = "${aws_api_gateway_rest_api.serverless_demos.id}"
+  resource_id   = "${aws_api_gateway_resource.serverless_demos_resource.id}"
+  http_method = "${aws_api_gateway_method.serverless_demos_method.http_method}"
+  status_code = "200"
 
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+}
 
+resource "aws_api_gateway_integration_response" "integration_response1" {
+  rest_api_id = "${aws_api_gateway_rest_api.serverless_demos.id}"
+  resource_id   = "${aws_api_gateway_resource.serverless_demos_resource.id}"
+  http_method = "${aws_api_gateway_method.serverless_demos_method.http_method}"
+  status_code = "200"
 
-
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+}
+# -------------End Of Cors Integration
 
 
 
