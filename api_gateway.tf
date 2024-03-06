c76eafda0654130320c970a8423c8fac90e64e37
satyamdudhat <satyam.glsica19@gmail.com>
2024-03-06 18:52:29 +0530

    Update a status Resource code

--- api_gateway.tf
+++ api_gateway.tf
@@ -41,27 +41,27 @@ resource "aws_lambda_permission" "apigw_lambda" {
 }
 
 # ---------------------CoRS Integration
-# resource "aws_api_gateway_method_response" "method_response1" {
-#   rest_api_id = "${aws_api_gateway_rest_api.serverless_demos.id}"
-#   resource_id   = "${aws_api_gateway_resource.serverless_demos_resource.id}"
-#   http_method = "${aws_api_gateway_method.serverless_demos_method.http_method}"
-#   status_code = "200"
+resource "aws_api_gateway_method_response" "method_response1" {
+  rest_api_id = "${aws_api_gateway_rest_api.serverless_demos.id}"
+  resource_id   = "${aws_api_gateway_resource.serverless_demos_resource.id}"
+  http_method = "${aws_api_gateway_method.serverless_demos_method.http_method}"
+  status_code = "200"
 
-#   response_parameters = {
-#     "method.response.header.Access-Control-Allow-Origin" = "'*'"
-#   }
-# }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+}
 
-# resource "aws_api_gateway_integration_response" "integration_response1" {
-#   rest_api_id = "${aws_api_gateway_rest_api.serverless_demos.id}"
-#   resource_id   = "${aws_api_gateway_resource.serverless_demos_resource.id}"
-#   http_method = "${aws_api_gateway_method.serverless_demos_method.http_method}"
-#   status_code = "200"
+resource "aws_api_gateway_integration_response" "integration_response1" {
+  rest_api_id = "${aws_api_gateway_rest_api.serverless_demos.id}"
+  resource_id   = "${aws_api_gateway_resource.serverless_demos_resource.id}"
+  http_method = "${aws_api_gateway_method.serverless_demos_method.http_method}"
+  status_code = "200"
 
-#   response_parameters = {
-#     "method.response.header.Access-Control-Allow-Origin" = true
-#   }
-# }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
 # -------------End Of Cors Integration
 
 
