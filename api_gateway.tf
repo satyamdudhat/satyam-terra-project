2c9cb895c09e6772d5720c356b88dc25133bced0
satyamdudhat <satyam.glsica19@gmail.com>
2024-03-06 22:40:49 +0530

    Update a status Resource code

--- api_gateway.tf
+++ api_gateway.tf
@@ -44,9 +44,9 @@ resource "aws_lambda_permission" "apigw_lambda" {
 
 # ---------------------CoRS Integration
 resource "aws_api_gateway_method_response" "method_response1" {
-  rest_api_id = "${aws_api_gateway_rest_api.serverless_demos.id}"
-  resource_id   = "${aws_api_gateway_resource.serverless_demos_resource.id}"
-  http_method = "${aws_api_gateway_method.serverless_demos_method.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.serverless_demos.id
+  resource_id = aws_api_gateway_resource.serverless_demos_resource.id
+  http_method = aws_api_gateway_method.serverless_demos_method.http_method
   status_code = "200"
 
   response_parameters = {
@@ -55,16 +55,16 @@ resource "aws_api_gateway_method_response" "method_response1" {
 }
 
 resource "aws_api_gateway_integration_response" "integration_response1" {
-  rest_api_id = "${aws_api_gateway_rest_api.serverless_demos.id}"
-  resource_id   = "${aws_api_gateway_resource.serverless_demos_resource.id}"
-  http_method = "${aws_api_gateway_method.serverless_demos_method.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.serverless_demos.id
+  resource_id = aws_api_gateway_resource.serverless_demos_resource.id
+  http_method = aws_api_gateway_method.serverless_demos_method.http_method
   status_code = "200"
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Origin" = "'*'"
   }
 
-    depends_on = [aws_api_gateway_integration.integration]
+  depends_on = [aws_api_gateway_integration.integration]
 }
 # -------------End Of Cors Integration
 
