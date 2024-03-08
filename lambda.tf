d1d53aed86c90f514b02cb052009607931e5390d
satyamdudhat <satyam.glsica19@gmail.com>
2024-03-08 11:20:44 +0530

    Update a status Resource code

--- lambda.tf
+++ lambda.tf
@@ -1,9 +1,9 @@
-# We create a lambda fucntion which access a ecr image
-resource "aws_lambda_function" "satyam_lambda_function" {
-  depends_on = [ null_resource.default ]
-  function_name    = "satyam_lambda_function"
-  architectures = ["x86_64"]
-  package_type     = "Image"
-  image_uri        = "${aws_ecr_repository.satyam_project.repository_url}:latest"
-  role             = aws_iam_role.satyam_lambda_execution_role.arn
-}
+# # We create a lambda fucntion which access a ecr image
+# resource "aws_lambda_function" "satyam_lambda_function" {
+#   depends_on = [ null_resource.default ]
+#   function_name    = "satyam_lambda_function"
+#   architectures = ["x86_64"]
+#   package_type     = "Image"
+#   image_uri        = "${aws_ecr_repository.satyam_project.repository_url}:latest"
+#   role             = aws_iam_role.satyam_lambda_execution_role.arn
+# }
--- scripts/run_command.sh
+++ scripts/run_command.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+# Get the current GitHub commit hash
+GITHUB_COMMIT_HASH=$(git rev-parse --short HEAD)
+
+# Docker image tag with GitHub commit hash
+DOCKER_IMAGE_TAG="satyam-image:${GITHUB_COMMIT_HASH}"
+
+# # Build the Docker image
+# docker build -t "${DOCKER_IMAGE_TAG}" .
+
+# # Push the Docker image to your Docker registry (e.g., Amazon ECR)
+# docker push "${DOCKER_IMAGE_TAG}"
+
+
+
 aws ecr get-login-password --region ap-south-1 --profile satyam | docker login --username AWS --password-stdin 637423557273.dkr.ecr.ap-south-1.amazonaws.com
-docker build -t satyam_project:latest .
-docker tag satyam_project:latest 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:latest
+docker build -t "${DOCKER_IMAGE_TAG}" .
+docker tag "${DOCKER_IMAGE_TAG}" 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:latest
 docker push 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:latest
 
 
