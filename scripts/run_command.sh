24f298c1303dbf243a074254bb8d6a43eef83e35
satyamdudhat <satyam.glsica19@gmail.com>
2024-03-14 16:45:30 +0530

    Update a status Resource code

--- scripts/run_command.sh
+++ scripts/run_command.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 GITHUB_COMMIT_HASH=$(git rev-parse --short HEAD)
-# ACCOUNT_ID=637423557273
-# IMAGE_NAME="satyam_fucntion1"
-
-# DOCKER_IMAGE_TAG="${GITHUB_COMMIT_HASH}"
 
 aws ecr get-login-password --region ap-south-1 --profile satyam | docker login --username AWS --password-stdin 637423557273.dkr.ecr.ap-south-1.amazonaws.com
 docker build -t satyam_fucntion1 .
