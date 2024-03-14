8c94fb1d806bef9fe6f0012dd88fd5a7b0b1ae20
satyamdudhat <satyam.glsica19@gmail.com>
2024-03-14 17:53:10 +0530

    Update a status Resource code

--- api_gateway.tf
+++ api_gateway.tf
@@ -40,10 +40,10 @@ resource "aws_api_gateway_integration" "integration" {
 resource "aws_lambda_permission" "apigw_lambda" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.satyam_lambda_function.arn
+  function_name = "${aws_lambda_function.satyam_lambda_function.function_name}"
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "arn:aws:execute-api:${aws_api_gateway_rest_api.serverless_demos.id}/*/${aws_api_gateway_method.serverless_demos_method.http_method}${aws_api_gateway_resource.serverless_demos_resource.path}"
+  source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.serverless_demos.id}/*/${aws_api_gateway_method.serverless_demos_method.http_method}${aws_api_gateway_resource.serverless_demos_resource.path}"
 }
 
 
