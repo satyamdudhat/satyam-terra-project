56c98548c3d049fd1d2b3504d4d8d2a35cb8baa2
satyamdudhat <satyam.glsica19@gmail.com>
2024-03-14 16:25:25 +0530

    Update a status Resource code

--- scripts/run_command.sh
+++ scripts/run_command.sh
@@ -2,17 +2,17 @@
 
 GITHUB_COMMIT_HASH=$(git rev-parse --short HEAD)
 # ACCOUNT_ID=637423557273
-IMAGE_NAME="satyam_fucntion1"
+# IMAGE_NAME="satyam_fucntion1"
 
 # DOCKER_IMAGE_TAG="${GITHUB_COMMIT_HASH}"
 
-aws ecr get-login-password --region ap-south-1 --profile satyam | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.ap-south-1.amazonaws.com
-docker build -t $IMAGE_NAME .
-docker tag $IMAGE_NAME $AWS_ACCOUNT_ID.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:latest
-docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:latest
+aws ecr get-login-password --region ap-south-1 --profile satyam | docker login --username AWS --password-stdin 637423557273.dkr.ecr.ap-south-1.amazonaws.com
+docker build -t satyam_fucntion1 .
+docker tag satyam_fucntion1 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:latest
+docker push 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:latest
 
-docker tag $IMAGE_NAME $AWS_ACCOUNT_ID.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:$GITHUB_COMMIT_HASH
-docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:$GITHUB_COMMIT_HASH
+docker tag satyam_fucntion1 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:$GITHUB_COMMIT_HASH
+docker push 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:$GITHUB_COMMIT_HASH
 
 
 
