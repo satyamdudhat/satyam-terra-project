21f6837314ed6718a531f43692a9656406a4f6ec
satyamdudhat <satyam.glsica19@gmail.com>
2024-03-15 14:00:04 +0530

    Update code

--- scripts/run_command.sh
+++ scripts/run_command.sh
@@ -12,3 +12,13 @@ docker push 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:$GITHUB
 
 
 
+
+# For Local run code
+
+# aws ecr get-login-password --region ap-south-1 --profile satyam | docker login --username AWS --password-stdin 637423557273.dkr.ecr.ap-south-1.amazonaws.com
+# docker build -t satyam_fucntion1 .
+# docker tag satyam_fucntion1 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:latest
+# docker push 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:latest
+
+# docker tag satyam_fucntion1 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:1.0
+# docker push 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:1.0
