26e485f0d58c4d0de8c1aa7290a857cb3e22dd7a
satyamdudhat <satyam.glsica19@gmail.com>
2024-03-18 11:20:44 +0530

    Update condition

--- lambda.tf
+++ lambda.tf
@@ -1,6 +1,5 @@
-# We create a lambda fucntion which access a ecr image
 resource "aws_lambda_function" "satyam_lambda_function" {
-  depends_on = [ null_resource.default ]
+  # depends_on = [ null_resource.default ]
   # depends_on = [ aws_ecr_repository.satyam_project ]
   function_name    = "satyam_lambda_function"
   architectures = ["x86_64"]
