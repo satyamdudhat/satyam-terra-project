0558a31afb62c62c06ab38249737780d8927f6da
satyamdudhat <satyam.glsica19@gmail.com>
2024-03-18 12:33:23 +0530

    Update condition

--- scripts/run_command.sh
+++ scripts/run_command.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 GITHUB_COMMIT_HASH=$(git rev-parse --short HEAD)
+AWS_ACCOUNT_ID="${AWS_ACCOUNT_ID}"
+AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION}"
+AWS_PROFILE="${AWS_PROFILE}"
 
 aws ecr get-login-password --region $AWS_DEFAULT_REGION --profile $AWS_PROFILE | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 docker build -t satyam_fucntion1 .
