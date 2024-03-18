5b9225154791c0ae663132e50ddb7804ea6cb53f
satyamdudhat <satyam.glsica19@gmail.com>
2024-03-18 12:28:18 +0530

    Update condition

--- scripts/run_command.sh
+++ scripts/run_command.sh
@@ -2,7 +2,7 @@
 
 GITHUB_COMMIT_HASH=$(git rev-parse --short HEAD)
 
-aws ecr get-login-password --region ap-south-1 --profile satyam | docker login --username AWS --password-stdin 637423557273.dkr.ecr.ap-south-1.amazonaws.com
+aws ecr get-login-password --region $AWS_DEFAULT_REGION --profile $AWS_PROFILE | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 docker build -t satyam_fucntion1 .
 docker tag satyam_fucntion1 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:latest
 docker push 637423557273.dkr.ecr.ap-south-1.amazonaws.com/satyam_project:latest
