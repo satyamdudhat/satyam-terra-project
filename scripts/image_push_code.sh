00202be0183b16d66a7c9b68b1907ee82875b775
satyamdudhat <satyam.glsica19@gmail.com>
2024-04-01 15:52:29 +0530

    Update api gateway file

--- scripts/image_push_code.sh
+++ scripts/image_push_code.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GITHUB_COMMIT_HASH=$(git rev-parse --short HEAD)
+
 
 echo "AWS Access Key ID: $AWS_ACCOUNT_ID"
 aws ecr get-login-password --region "${AWS_DEFAULT_REGION}" --profile "${AWS_PROFILE}" | docker login --username AWS --password-stdin "${AWS_ACCOUNT_ID}".dkr.ecr."${AWS_DEFAULT_REGION}".amazonaws.com
@@ -8,5 +8,5 @@ docker build -t satyam_fucntion1 .
 docker tag satyam_fucntion1 "${AWS_ACCOUNT_ID}".dkr.ecr."${AWS_DEFAULT_REGION}".amazonaws.com/satyam_project:latest
 docker push "${AWS_ACCOUNT_ID}".dkr.ecr."${AWS_DEFAULT_REGION}".amazonaws.com/satyam_project:latest
 
-docker tag satyam_fucntion1 "${AWS_ACCOUNT_ID}".dkr.ecr."${AWS_DEFAULT_REGION}".amazonaws.com/satyam_project:$GITHUB_COMMIT_HASH
-docker push "${AWS_ACCOUNT_ID}".dkr.ecr."${AWS_DEFAULT_REGION}".amazonaws.com/satyam_project:$GITHUB_COMMIT_HASH
+docker tag satyam_fucntion1 "${AWS_ACCOUNT_ID}".dkr.ecr."${AWS_DEFAULT_REGION}".amazonaws.com/satyam_project:"${GITHUB_COMMIT_HASH}"
+docker push "${AWS_ACCOUNT_ID}".dkr.ecr."${AWS_DEFAULT_REGION}".amazonaws.com/satyam_project:"${GITHUB_COMMIT_HASH}"
