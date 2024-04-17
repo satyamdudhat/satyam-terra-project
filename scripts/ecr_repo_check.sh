3ec5c8aa88f37f47e46fb301672fe39d3eb5078d
satyamdudhat <satyam.glsica19@gmail.com>
2024-04-17 16:34:20 +0530

    Final

--- scripts/ecr_repo_check.sh
+++ scripts/ecr_repo_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if aws ecr describe-repositories --repository-names "satyam_project" --profile satyam &> /dev/null; then
+if aws ecr describe-repositories --repository-names "satyam_project" --profile "${AWS_PROFILE}" &> /dev/null; then
     echo "ECR repository exists"
     echo "::set-output name=exists::true"
 else
