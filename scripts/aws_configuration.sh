7256d4a20c33ed507d3ec4ef5b93da2c48d838c4
satyamdudhat <satyam.glsica19@gmail.com>
2024-04-17 09:47:52 +0530

    stage env added

---
+++ scripts/aws_configuration.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+aws configure set aws_access_key_id "${AWS_ACCESS_KEY_ID}" --profile satyam
+aws configure set aws_secret_access_key "${AWS_SECRET_ACCESS_KEY}" --profile satyam
+aws configure set region "${AWS_DEFAULT_REGION}" --profile satyam
+echo "AWS Access Key ID: $AWS_ACCESS_KEY_ID"
