f8d99983c4e149bd6b02d220f052cca8e723a906
satyamdudhat <satyam.glsica19@gmail.com>
2024-04-23 10:56:21 +0530

    Update workspace

--- scripts/ecr_repo_check.sh
+++ scripts/ecr_repo_check.sh
@@ -4,5 +4,6 @@ if aws ecr describe-repositories --repository-names "satyam_project" --profile "
     echo "ECR repository exists"
     echo "::set-output name=exists::true"
 else
+    terraform init
     terraform apply -auto-approve -target=aws_ecr_repository.satyam_project
 fi
