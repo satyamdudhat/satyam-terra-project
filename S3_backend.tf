9465d2787ab3afd2524f24b1a61dd21c0def7a80
satyamdudhat <satyam.glsica19@gmail.com>
2024-04-23 16:00:39 +0530

    Update workspace

--- S3_backend.tf
+++ S3_backend.tf
@@ -1,10 +1,17 @@
 terraform {
   backend "s3" {
-    bucket = "satyam-terra-backend-1"
-    key    = "devs/terraform.tfstate"   
+    # bucket = "satyam-terra-backend-1"
+    # key    = "terraform.tfstate"
+    bucket = var.bucket_name
+    key    = var.key_name
     region = "ap-south-1"
     profile = "satyam"
   }
 }
 
+
+
+variable "bucket_name" {}
+
+variable "key_name" {}
 
