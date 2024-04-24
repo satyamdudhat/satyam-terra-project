2b424bb4d0b17159d14f38070f67e2efff3e2f23
satyamdudhat <satyam.glsica19@gmail.com>
2024-04-24 11:59:12 +0530

    Update workspace

--- scripts/ecr_repo_check_devs.sh
+++ scripts/ecr_repo_check_devs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if aws ecr describe-repositories --repository-names "satyam_project_devs" --profile "${AWS_PROFILE}" &> /dev/null; then
+if aws ecr describe-repositories --repository-names "project_devs" --profile "${AWS_PROFILE}" &> /dev/null; then
     echo "ECR repository exists"
     echo "::set-output name=exists::true"
 else
